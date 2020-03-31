f1a8536addca154c5a897a3c6d7a18e706539d3c
Ahmed ElNahas <ahmedelnahas@MacBook-Pro-2.local>
2020-03-31 00:04:03 -0400

    removing password encoding

--- functions/app/services/database-service.js
+++ functions/app/services/database-service.js
@@ -3,12 +3,7 @@ import config from "../config"
 
 const makeConnectionString = () => {
     const { dbUser, dbPassword, dbHost, dbName } = config
-
-    // encoding username and password to handle special chars in conn string
-    const encodedUser = encodeURI(dbUser)
-    const encodedPassword = encodeURI(dbPassword)
-
-    return `mongodb+srv://${encodedUser}:${encodedPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+    return `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
 }
 
 
