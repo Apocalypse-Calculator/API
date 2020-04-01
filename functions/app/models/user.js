862dc7214664718d14615443696876e4f039b2c6
Ahmed ElNahas <ahmedelnahas@MacBook-Pro-2.local>
2020-03-31 20:35:57 -0400

    define user schema

---
+++ functions/app/models/user.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const userCollection = 'users';
+
+export const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: null },
+  confirmedAt: { type: Date, default: null },
+  confirmationSentAt: { type: Date, default: null },
+  location: {
+    city: String,
+    country: String,
+  },
+});
