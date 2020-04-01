02e87e9a9466a19ab1112896d3cf03cf4f7e6053
Ahmed ElNahas <ahmedelnahas@MacBook-Pro-2.local>
2020-03-31 22:10:06 -0400

    setup passport

--- functions/app/index.js
+++ functions/app/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import * as routes from "./controllers"
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import * as routes from './controllers';
 
-const createApp = (functionName) => {
+const createApp = functionName => {
   const app = express();
   const router = express.Router();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(passport.initialize());
 
   const routerBasePath = `/.netlify/functions/${functionName}`;
 
-  router.use("/ping", routes.getPingRoutes())
-  router.use("/stocks", routes.createStocksRoutes())
-  router.use("/estimations", routes.getEstimationRoutes())
-  router.use("/users", routes.getUserRoutes())
+  router.use('/ping', routes.getPingRoutes());
+  router.use('/stocks', routes.createStocksRoutes());
+  router.use('/estimations', routes.getEstimationRoutes());
+  router.use('/users', routes.getUserRoutes());
 
   app.use(routerBasePath, router);
 
--- functions/app/models/user.js
+++ functions/app/models/user.js
@@ -5,6 +5,9 @@ const userCollection = 'users';
 export const UserSchema = new mongoose.Schema({
   email: String,
   password: String,
+  displayName: String,
+  provider: String,
+  providerId: String,
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: null },
   confirmedAt: { type: Date, default: null },
