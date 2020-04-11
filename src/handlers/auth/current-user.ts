4f4926f0a5e33239729d6b5c0b8e2f98f39539d7
Ahmed ElNahas <ahmedelnahas@MacBook-Pro-2.local>
2020-04-11 10:58:59 -0400

    hide password in response

--- src/handlers/auth/current-user.ts
+++ src/handlers/auth/current-user.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import { UserRepository } from '~/src/storage';
+import { IUser } from '~/src/models';
 
 export const getCurrentUser = async (req: Request, resp: Response) => {
-  return resp.json({ user: req.user });
+  return resp.json({ user: UserRepository.serialize(req.user as IUser) });
 };
