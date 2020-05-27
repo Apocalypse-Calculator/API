31ad0a39942b3816bb64f58192518af604a3b8b8
Ahmed ElNahas <elnahas.ahmed@gmail.com>
2020-05-26 22:50:36 -0400

    set deleted to true on deleted item definition

--- src/storage/repositories/item-definitions/delete.ts
+++ src/storage/repositories/item-definitions/delete.ts
@@ -1,15 +1,11 @@
 import { ItemDefinition } from '~/src/models';
 
 export const deleteDefinition = async (id: string) => {
-  await ItemDefinition.findByIdAndUpdate(
-    id,
-    { deleted: false },
-    { upsert: false }
-  );
+	await ItemDefinition.findByIdAndUpdate(id, { deleted: true }, { upsert: false });
 };
 
 export const deleteUnit = async (id: string, unitId: string) => {
-  await ItemDefinition.findByIdAndUpdate(id, {
-    $pull: { units: { _id: unitId } },
-  });
+	await ItemDefinition.findByIdAndUpdate(id, {
+		$pull: { units: { _id: unitId } }
+	});
 };
